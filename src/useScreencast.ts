968dc9de121ae81147a55a7e0a67b0c492fa097f
Konrad Bochnia <kbochnia@gmail.com>
2023-07-28 07:50:10 +0200

    Fix the cleanup function

--- src/useScreencast.ts
+++ src/useScreencast.ts
@@ -38,11 +38,13 @@ export const SCREENSHARING_MEDIA_CONSTRAINTS: MediaStreamConstraints = {
 
 export function useScreencast({constraints, initOnMount = false}: UseDisplayMediaConfig): UseDisplayMedia {
     const [state, setState] = useState<UseDisplayMediaState>(DEFAULT_STATE);
+    const mediaRef = useRef<MediaStream>();
     const skip = useRef(false);
     
     const init: () => Promise<MediaStream | null> = useCallback(async () => {
         if (state.stream !== null || constraints === null) return null;
         const media = await navigator.mediaDevices.getDisplayMedia(constraints)
+        mediaRef.current = media;
         setState(prevState => ({
             ...prevState,
             stream: media,
@@ -54,6 +56,7 @@ export function useScreencast({constraints, initOnMount = false}: UseDisplayMedi
     const stop: () => Promise<void> = useCallback(async () => {
         if (state.stream === null) return;
         stopTracks(state.stream);
+        mediaRef.current = undefined;
         setState(prevState => ({
             ...prevState,
             stream: null,
@@ -70,24 +73,24 @@ export function useScreencast({constraints, initOnMount = false}: UseDisplayMedi
             ...prevState,
             isEnabled: status
         }));
-    }, [state.stream?.id]);
+    }, [state.stream, state.stream?.id]);
     
     useEffect(() => {
-        if (skip.current) return;
-        skip.current = true;
-        
-        if (initOnMount) {
+        if (!skip.current && initOnMount) {
             init();
         }
-        
+        skip.current = true;
+
         return function cleanup() {
-            if (state.stream) {
-                stopTracks(state.stream);
-                setState(prevState => ({
-                    ...prevState,
-                    stream: null
-                }))
-            }
+            if (!mediaRef.current) return;
+            const media = mediaRef.current;
+            stopTracks(media);
+            mediaRef.current = undefined;
+            setState(prevState => ({
+                ...prevState,
+                stream: null,
+                isEnabled: false
+            }))
         }
     }, []);
     
