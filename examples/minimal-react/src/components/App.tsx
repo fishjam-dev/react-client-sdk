e1855344ef0666ce4faa2ef23615ccc991828809
Kamil Stasiak <kamilstasiaknew@gmail.com>
2023-08-25 11:10:13 +0200

    Add useDisconnect and some styling

--- examples/minimal-react/src/components/App.tsx
+++ examples/minimal-react/src/components/App.tsx
@@ -15,7 +15,7 @@ export type TrackMetadata = {
 
 // Create a Membrane client instance
 // remember to use JellyfishContextProvider
-export const { useApi, useTracks, useStatus, useConnect, JellyfishContextProvider } = create<
+export const { useApi, useTracks, useStatus, useConnect, useDisconnect, JellyfishContextProvider } = create<
   PeerMetadata,
   TrackMetadata
 >();
@@ -24,37 +24,49 @@ export const App = () => {
   const [token, setToken] = useState("");
 
   const connect = useConnect();
+  const disconnect = useDisconnect();
   const api = useApi();
   const status = useStatus();
   const tracks = useTracks();
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
       <input value={token} onChange={(e) => setToken(() => e?.target?.value)} placeholder="token" />
-      <button
-        onClick={() => {
-          if (!token || token === "") throw Error("Token is empty");
-          connect({
-            peerMetadata: { name: "John Doe" }, // example metadata
-            token: token,
-          });
-        }}
-      >
-        Join
-      </button>
-      <button
-        disabled={status !== "joined"}
-        onClick={() => {
-          // Get screen sharing MediaStream
-          navigator.mediaDevices.getDisplayMedia(SCREEN_SHARING_MEDIA_CONSTRAINTS).then((screenStream) => {
-            // Add local MediaStream to webrtc
-            screenStream.getTracks().forEach((track) => api.addTrack(track, screenStream, { type: "screen" }));
-          });
-        }}
-      >
-        Start screen share
-      </button>
-      <span>Status: {status}</span>
+      <div style={{ display: "flex", flexDirection: "row", gap: "8px" }}>
+        <button
+          disabled={token === "" || status === "joined"}
+          onClick={() => {
+            if (!token || token === "") throw Error("Token is empty");
+            connect({
+              peerMetadata: { name: "John Doe" }, // example metadata
+              token: token,
+            });
+          }}
+        >
+          Connect
+        </button>
+        <button
+          disabled={status !== "joined"}
+          onClick={() => {
+            disconnect();
+          }}
+        >
+          Disconnect
+        </button>
+        <button
+          disabled={status !== "joined"}
+          onClick={() => {
+            // Get screen sharing MediaStream
+            navigator.mediaDevices.getDisplayMedia(SCREEN_SHARING_MEDIA_CONSTRAINTS).then((screenStream) => {
+              // Add local MediaStream to webrtc
+              screenStream.getTracks().forEach((track) => api.addTrack(track, screenStream, { type: "screen" }));
+            });
+          }}
+        >
+          Start screen share
+        </button>
+        <span>Status: {status}</span>
+      </div>
       {/* Render the remote tracks from other peers*/}
       {Object.values(tracks).map(({ stream, trackId }) => (
         <VideoPlayer key={trackId} stream={stream} /> // Simple component to render a video element
